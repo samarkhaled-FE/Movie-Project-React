5edb01619c863ca7f2196bbe3ef58ffdaccdf998
samarkhaled-FE <samarkhaledabd100@gmail.com>
2024-12-22 21:16:19 +0300

    Updates

---
+++ static/js/952.5d18b2a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app2=self.webpackChunkmy_app2||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5d18b2a3.chunk.js.map
